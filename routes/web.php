d6467ee0cf2030dd51358444f9cc714312c39a1b
bn4t <me@bn4t.me>
2021-01-13 20:17:05 +0100

    revert test

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['throttle:global'])->group(function () {
-    Route::get('/ip', function (Request $request) {
-        dd($request->ip());
-    });
     Route::get('/signup/{date}', [AttendeeController::class, 'create'])->name('attendees.create');
 
     Route::resource('events', EventController::class);
