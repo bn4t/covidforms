cc4cf58c09662cfee82eb42c678848b74a4c3ef2
bn4t <me@bn4t.me>
2021-01-14 13:07:22 +0100

    styling fixes

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -146,7 +146,7 @@
                     <line x1="15" y1="9" x2="9" y2="15"></line>
                     <line x1="9" y1="9" x2="15" y2="15"></line>
                 </svg>
-                <p class="text-center text-xl text-gray-600 mt-3">Noch keine Anmeldungen.</p>
+                <p class="text-center text-xl text-gray-600 mt-3 mb-20">Noch keine Anmeldungen.</p>
             </div>
 
         @elseif(count($attendees) == 0)
@@ -158,7 +158,7 @@
                     <line x1="15" y1="9" x2="9" y2="15"></line>
                     <line x1="9" y1="9" x2="15" y2="15"></line>
                 </svg>
-                <p class="text-center text-xl text-gray-600 mt-3">Keine Anmeldungen, welche dem Filter entsprechen.</p>
+                <p class="text-center text-xl text-gray-600 mt-3 mb-20">Keine Anmeldungen, welche dem Filter entsprechen.</p>
             </div>
         @else
             <div class="max-w-6xl mx-auto">
