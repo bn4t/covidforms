e1971c780addfbffe3c92d472ae4e3baec1e0072
bn4t <me@bn4t.me>
2021-02-20 15:06:26 +0100

    more UI improvements

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -5,7 +5,10 @@
                 {{ __('Dashboard') }}
             </h2>
             <div class="flex items-center">
-                <a href="{{ route('events.create') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg mr-2">Neuer Event</a>
+                <a href="{{ route('events.create') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg mr-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                    <span class="ml-2">Neuer Event</span>
+                </a>
             </div>
         </div>
     </x-slot>
