fd8c44fdee46b5c3b05dc56821db6c6c941ba873
bn4t <me@bn4t.me>
2021-02-20 14:55:43 +0100

    improve UI

--- resources/views/notification_settings/edit.blade.php
+++ resources/views/notification_settings/edit.blade.php
@@ -2,12 +2,15 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Benachrichtigungseinstellungen ändern - ').$event->title }}
+                {{ __('Benachrichtigungseinstellungen - ').$event->title }}
             </h2>
         </div>
     </x-slot>
 
     <div class="mt-10 max-w-3xl mx-auto px-5 pb-10">
+
+        <h1 class="text-3xl mb-1">Benachrichtigungseinstellungen</h1>
+        <h2 class="text-lg mb-10">Event: {{$event->title}}</h2>
 
         @if ($errors->any())
             <div
@@ -21,25 +24,35 @@
         @endif
 
 
-        <form method="post" class="max-w-md mx-auto"  action="{{ route('notification_settings.store', $event) }}">
+        <form method="post" class="max-w-md mx-auto" action="{{ route('notification_settings.store', $event) }}">
             @csrf
             <div class="flex items-center mb-3">
-                <input type="checkbox" id="notify_adults" name="notify_adults" class="rounded p-2 mr-2" value="1" {{ old('notify_adults') ?? ($notify_adults == true ? 'checked="checked"' : '') }}">
+                <input type="checkbox" id="notify_adults" name="notify_adults" class="rounded p-2 mr-2"
+                       value="1" {{ old('notify_adults') ?? ($notify_adults == true ? 'checked="checked"' : '') }}">
                 <label for="notify_adults" class="text-xl flex-1">Anmeldungen von Erwachsenen</label>
             </div>
             <div class="flex items-center mb-3">
-                <input type="checkbox" id="notify_children_old" name="notify_children_old" class="rounded p-2 mr-2" value="1"  {{ old('notify_children_old') ?? ($notify_children_old == true ? 'checked="checked"' : '') }}">
+                <input type="checkbox" id="notify_children_old" name="notify_children_old" class="rounded p-2 mr-2"
+                       value="1" {{ old('notify_children_old') ?? ($notify_children_old == true ? 'checked="checked"' : '') }}
+                ">
                 <label for="notify_children_old" class="text-xl flex-1">Anmeldungen von Kindern (2. KL. - 6. KL)</label>
             </div>
             <div class="flex items-center mb-3">
-                <input type="checkbox" id="notify_children_young" name="notify_children_young" class="rounded p-2 mr-2" value="1"  {{ old('notify_children_young') ?? ($notify_children_young == true ? 'checked="checked"' : '') }}">
-                <label for="notify_children_young" class="text-xl flex-1">Anmeldungen von Kindern (3 Jahre - 1. KL)</label>
+                <input type="checkbox" id="notify_children_young" name="notify_children_young" class="rounded p-2 mr-2"
+                       value="1" {{ old('notify_children_young') ?? ($notify_children_young == true ? 'checked="checked"' : '') }}
+                ">
+                <label for="notify_children_young" class="text-xl flex-1">Anmeldungen von Kindern (3 Jahre - 1.
+                    KL)</label>
             </div>
             <div class="flex items-center mb-3">
-                <input type="checkbox" id="notify_babies" name="notify_babies" class="rounded p-2 mr-2" value="1"  {{ old('notify_babies') ?? ($notify_babies == true ? 'checked="checked"' : '') }}">
+                <input type="checkbox" id="notify_babies" name="notify_babies" class="rounded p-2 mr-2"
+                       value="1" {{ old('notify_babies') ?? ($notify_babies == true ? 'checked="checked"' : '') }}">
                 <label for="notify_babies" class="text-xl flex-1">Anmeldungen von Kleinkindern (0 - 3 Jahre)</label>
             </div>
-            <button class="mx-auto block bg-gray-800 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg text-xl mt-12">Speichern</button>
+            <button
+                class="mx-auto block bg-gray-800 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg text-xl mt-12">
+                Speichern
+            </button>
         </form>
 
     </div>
