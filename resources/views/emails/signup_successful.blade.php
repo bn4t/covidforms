790c0d1ccf0d32b9620b6086bebb569cb8c4183a
bn4t <me@bn4t.me>
2021-02-20 14:48:56 +0100

    don't escape spaces in markdown email templates

--- resources/views/emails/signup_successful.blade.php
+++ resources/views/emails/signup_successful.blade.php
@@ -18,16 +18,16 @@ Datum: {{ \Carbon\Carbon::parse($event->date)->format('d.m.Y') }}
 @foreach($attendees as $att)
 @switch($att->type)
 @case('adult')
-| {{ addcslashes($att->first_name , '\_ `*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_ `*[]()#+-.!') }}| Erwachsener | {{ addcslashes($att->comment , '\_ `*[]()#+-.!') }} |
+| {{ addcslashes($att->first_name , '\_`*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_`*[]()#+-.!') }}| Erwachsener | {{ addcslashes($att->comment , '\_`*[]()#+-.!') }} |
 @break
 @case('child_old')
-| {{ addcslashes($att->first_name , '\_ `*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_ `*[]()#+-.!') }} | Kind (2. Kl. - 6. Kl.) | {{ addcslashes($att->comment , '\_ `*[]()#+-.!') }} |
+| {{ addcslashes($att->first_name , '\_`*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_`*[]()#+-.!') }} | Kind (2. Kl. - 6. Kl.) | {{ addcslashes($att->comment , '\_`*[]()#+-.!') }} |
 @break
 @case('child_young')
-| {{ addcslashes($att->first_name , '\_ `*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_ `*[]()#+-.!') }} | Kind (3 Jahre - 1. Kl.) | {{ addcslashes($att->comment , '\_ `*[]()#+-.!') }} |
+| {{ addcslashes($att->first_name , '\_`*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_`*[]()#+-.!') }} | Kind (3 Jahre - 1. Kl.) | {{ addcslashes($att->comment , '\_`*[]()#+-.!') }} |
 @break
 @case('baby')
-| {{ addcslashes($att->first_name , '\_ `*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_ `*[]()#+-.!') }} | Kleinkind (0 - 3 Jahre) | {{ addcslashes($att->comment , '\_ `*[]()#+-.!') }} |
+| {{ addcslashes($att->first_name , '\_`*[]()#+-.!') }} | {{ addcslashes($att->last_name , '\_`*[]()#+-.!') }} | Kleinkind (0 - 3 Jahre) | {{ addcslashes($att->comment , '\_`*[]()#+-.!') }} |
 @break
 @endswitch
 @endforeach
