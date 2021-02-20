226734d7880c8e7af4656c8079af2c8bc8c58efd
bn4t <me@bn4t.me>
2021-02-20 15:03:20 +0100

    UI improvements

--- resources/views/notification_settings/edit.blade.php
+++ resources/views/notification_settings/edit.blade.php
@@ -4,6 +4,13 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Benachrichtigungseinstellungen - ').$event->title }}
             </h2>
+            <div class="flex items-center">
+                <a href="{{ route('events.show', $event) }}"
+                   class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg mr-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+                    <span class="ml-1">Zurück</span>
+                </a>
+            </div>
         </div>
     </x-slot>
 
