21dd68246f1f15ed7059afb4a8ce50d3eaca3290
bn4t <me@bn4t.me>
2021-09-03 10:49:31 +0200

    add sentry support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,16 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://007ef49fd9424f86be4cd0828547e446@o987414.ingest.sentry.io/5944287",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
 require('./bootstrap');
 
 import Alpine from 'alpinejs'
