bbd5358be7b98f72c95bb4b51f6a1cbeca1545e4
bn4t <me@bn4t.me>
2021-10-25 20:39:42 +0200

    add doctrine

--- database/migrations/2021_10_25_183230_event_date_non_unique.php
+++ database/migrations/2021_10_25_183230_event_date_non_unique.php
@@ -14,7 +14,7 @@ class EventDateNonUnique extends Migration
     public function up()
     {
         Schema::table('events', function(Blueprint $table) {
-            $table->date('date')->unique(false)->nullable()->change();
+            $table->dropUnique('date');
         });
     }
 
